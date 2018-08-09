9e1def66687cbed2ab5be2b108c2359163a42ec5
seanraad123 <seanconrad123@gmail.com>
2018-08-09 14:39:43 -0400

    added spaceship

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,20 @@
-class Spaceship {}
+let spaceShipID = 0;
+
+class Spaceship  {
+    constructor(name, crewMembers, phasers, shields ){
+      this.id = ++ spaceShipID
+      this.name = name
+      this.crewMembers = crewMembers
+      this.phasers = phasers
+      this.shields = shields
+      this.cloaked = false
+      this.warpDrive = 'disengaged'
+      if (crewMembers.length === 0){
+        this.docked = true
+      }
+      else if(crewMembers.length > 0) {
+        this.docked = false
+      }
+      this.phasersCharge = 'uncharged'
+    }
+}
